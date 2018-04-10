f413b27c5f5d9ed19d4b952243a80489924ede5b
Annie Gao <anniegao2007@gmail.com>
2018-04-10 19:54:11 -0400

    sections working?

---
+++ models/sections.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const stringField = {
+    type: String,
+    minlength: 1,
+    maxlength: 50,
+};
+
+const SectionSchema = new Schema({
+    instructor: stringField, //id of teacher
+    name: stringField,
+    students: [String], //ids of students
+    classId: String, //id of parent class
+});
+
+module.exports = mongoose.model('Sections', SectionSchema);
